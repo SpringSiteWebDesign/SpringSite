fc6660d60858577dd6d09759a94c16a9279673a4
Sean Novak <66228745+sn0va@users.noreply.github.com>
2024-01-14 17:12:53 -0700

    created modules

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,10 +17,10 @@
       // === logo change
       if (ud_header.classList.contains("sticky")) {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/Logo.svg"
+          "assets/images/logo/logoNew.svg"
       } else {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/Logo.svg"
+        "assets/images/logo/logoWhite.svg"
       }
     }
 
@@ -29,7 +29,7 @@
         // === logo change
         if (ud_header.classList.contains("sticky")) {
           document.querySelector(".header-logo").src =
-            "assets/images/logo/Logo.svg"
+          "assets/images/logo/logoNew.svg"
         } 
       }
     }
